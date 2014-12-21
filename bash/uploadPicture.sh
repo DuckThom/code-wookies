6ef39a90ba1efbc0f5c408fb4b735c052d26e173
Thomas Wiringa <thomas.wiringa@gmail.com>
2014-12-21 01:50:35 +0100

    Added some comments

--- bash/uploadPicture.sh
+++ bash/uploadPicture.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Usage: uploadPicture.sh <filename>
+############################################################
+# Usage: uploadPicture.sh <filename>                       #
+# 							   #
+# Use with the apple terminal, this is NOT a linux script! #
+############################################################
 
 FILE="$1"
-NAME="$(osascript -e 'Tell application "System Events" to display dialog "Enter the image name:" default answer ""' -e 'text returned of result' 2>/dev/null)"
+
+# Show a message asking for a file name
+NAME="$(osascript -e 'Tell application "System Events" to display dialog "Enter the image name (optional):" default answer ""' -e 'text returned of result' 2>/dev/null)"
 
 if [ $FILE != "" ]; then
 	curl -F image=@"$FILE" -F name="$NAME" -F key="$UPLOAD_KEY" http://lunamoonfang.nl/s/upload | pbcopy
