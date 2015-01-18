cdd9055c693545756219e90ab853327950a44788
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-01-18 20:50:07 +0100

    Fixed file naming

--- bash/uploadPicture.sh
+++ bash/uploadPicture.sh
@@ -12,7 +12,7 @@ KEY="$(cat $HOME/.uploadkey)"
 # Show a message asking for a file name
 NAME="$(osascript -e 'Tell application "System Events" to display dialog "Enter the image name (optional):" default answer ""' -e 'text returned of result' 2>/dev/null)"
 
-if [ $FILE != "" ]; then
+if [ -f "$FILE" ]; then
 	curl -F image=@"$FILE" -F name="$NAME" -F key="$KEY" http://lunamoonfang.nl/s/upload | pbcopy
 else
 	echo "Usage: uploadPicture.sh <filename>"
