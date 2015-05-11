1c5c0f4946e922abb42ff69cab0d4cb9bf40753a
Thomas Wiringa <thomas.wiringa@gmail.com>
2015-05-11 12:42:05 +0200

    Added default config generation + user input

--- python/lunabot.py
+++ python/lunabot.py
@@ -11,24 +11,37 @@ import urllib
 import threading
 from bs4 import BeautifulSoup
 
+running = True
+
 HOME = os.path.expanduser("~")
 
 cp = ConfigParser.RawConfigParser()
-cp.read(HOME + "/.lunabot/config")
-
+				
 # The actual bot code
 def startBot():
+	global running
+
+	cp.read(HOME + "/.lunabot/config")
 	
 	# Set some vaiables
+	# Strings
 	sNICK 		= cp.get('config', 'nickname')
 	sPASS 		= cp.get('config', 'password')
 	sCHAN 		= cp.get('config', 'channel')
+	
+	# Booleans
 	bKILL 		= cp.get('config', 'enableKill')
 	bINVALIDOUTPUT  = cp.get('config', 'invalidOutput')
+	
+	# Lists
 	lCOMMANDS 	= ast.literal_eval(cp.get('config', 'commands'))
+	lADMINS 	= ast.literal_eval(cp.get('config', 'admins'))
+
+	# Dictionary
 	dCOMMANDTYPES   = dict(cp.items("commandTypes"))
 	dMESSAGES	= dict(cp.items("messages"))
 	dSYSTEMCOMMANDS = dict(cp.items("systemCommands"))
+	
 	initial_response= connected_response = identify_response = read_line = ""
 
 	# Set the server stuff
@@ -73,7 +86,7 @@ def startBot():
 	userInput.start()
 
 	# Loop through the input handling
-	while True:
+	while running:
 		# Read the input from the socket
 		read_line = s.recv(1024)
 		message = ''		
@@ -90,8 +103,8 @@ def startBot():
 
 			if command in lCOMMANDS:
 		
-				if command == "kill" and user == "Luna_Moonfang" and bKILL == "1":
-					break # Stop the infinite loop
+				if command == "kill" and user in lADMINS and bKILL == "1":
+					running = False # Stop the infinite loop
 				
 				if dCOMMANDTYPES[command] == "silent":
 					output = 0
@@ -101,8 +114,7 @@ def startBot():
 			
 					message += os.popen(dSYSTEMCOMMANDS[command]).read()
 				elif dCOMMANDTYPES[command] == "text":
-					message += dMESSAGES[command]
-				
+					message += dMESSAGES[command]				
 
 			else:
 				if bINVALIDOUTPUT == "1":
@@ -140,32 +152,85 @@ def startBot():
 	# Disconnect from the IRC server
 	s.send("QUIT :Going back to the moon! \n")
 
-	# Close the connection
-	s.close()
-
-	print("Closed socket! Bye Bye o/ ")
+	time.sleep(1)
 # END DEF 
 
 def userInput():
+	global running
+
 	print "User input enabled!"
 
-	while True:
-		input = raw_input()
-		s.send(input + "\n")
+	while running:
+		raw_cmd = raw_input()
+		s.send(raw_cmd + "\n")
+
+		if re.match("QUIT", raw_cmd):
+			running = False
+	# END WHILE
+# END DEF
+
+def createConfig():
+	cp.add_section('config')
+	cp.set('config', 'nickname', 'The name the bot should use')
+	cp.set('config', 'password', 'Password to identify with NickServ')
+	cp.set('config', 'channel', 'The channel to connect to')
+	cp.set('config', 'enableKill', 'Allow the bot to be killed with !kill from chat ; 0 or 1')
+	cp.set('config', 'invalidOutput', 'Enable command not found response ; 0 or 1')
+	cp.set('config', 'commands', 'Enter the command names here ; ex: ["command1", "command2"]')
+	cp.set('config', 'admins', 'Enter the names of the admins ; ex: ["admin1", "admin2"]')
+
+	cp.add_section('commandTypes')
+	cp.set('commandTypes', 'command name', 'Command type ; text, system, silent')
+	cp.set('commandTypes', 'kill', 'silent')
+
+	cp.add_section('messages')
+	cp.set('messages', 'command1', 'This is an example message ; Message is optional for system type commands')
+
+	cp.add_section('systemCommands')
+	cp.set('systemCommands', 'command2', 'System command ; ex: ls or mkdir')
+
+	if not os.path.exists(HOME + "/.lunabot/"):
+		os.mkdir(HOME + "/.lunabot")
+
+	# Writing our configuration file to 'example.cfg'
+	with open(HOME + '/.lunabot/config', 'w+b') as configfile:
+		cp.write(configfile)
+
+	configfile.close()
+
+	print "Config file generated, closing bot. Please edit the config file located in ~/.lunabot"
+# END DEF
 
 try:
+	if not os.path.exists(HOME + "/.lunabot/config"):
+		print "Config file not found, creating..."
+		createConfig()
+		exit()
+	
 	s = socket.create_connection(("irc.freenode.net", 6667))
 	
 	bot = threading.Thread(target=startBot)
 	userInput = threading.Thread(target=userInput)	
 
-	bot.daemon = userInput.daemon = True
+	bot.daemon = True
+	userInput.daemon = True
 		
 	bot.start()
 
 	# While the bot is running, wait here...
-	while bot.isAlive: bot.join(5)	
+	bot.join()
+	
+	# Close the connection	
+	s.close()
+	sys.exit()
+
+	print "Connection closed"
 except KeyboardInterrupt:
 	print "CTRL-C found, exiting..."
-	s.send("QUIT :Oh noes! Someone killed my script! \n")
-	s.close()
+	
+	if bot.isAlive:
+		s.send("QUIT :Oh noes! Someone killed my script! \n")
+		time.sleep(1)
+		s.close()
+	
+	sys.exit()
