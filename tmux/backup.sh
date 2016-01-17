e970cde38162c4abf6463673f07c7106bfe5b6c4
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-01-17 22:49:15 +0100

    Actually backup and restore the tmux things

--- tmux/backup.sh
+++ tmux/backup.sh
@@ -2,14 +2,14 @@
 
 if [ -d "$HOME/.tmux" ]; then
 	echo "Copying .tmux folder to $PWD"
-	cp -r ~/.vim "$PWD"
+	cp -r ~/.tmux "$PWD"
 else
 	echo "No .tmux folder found in $HOME"
 fi
 
 if [ -f "$HOME/.tmux.conf" ]; then
 	echo "Copying .tmux.conf file to $PWD"
-	cp ~/.vimrc "$PWD"
+	cp ~/.tmux.conf "$PWD"
 else
 	echo "No .tmux.conf file found in $HOME"
 fi
--- tmux/restore.sh
+++ tmux/restore.sh
@@ -2,14 +2,14 @@
 
 if [ -d ".tmux" ]; then
 	echo "Copying .tmux config folder to $HOME"
-	cp -r .vim "$HOME"
+	cp -r .tmux "$HOME"
 else
 	echo "No .tmux config folder found in $PWD"
 fi
 
 if [ -f ".tmux.conf" ]; then
 	echo "Copying .tmux.conf file to $HOME"
-	cp .vimrc "$HOME"
+	cp .tmux.conf "$HOME"
 else
 	echo "No .tmux.conf config file found in $PWD"
 fi
