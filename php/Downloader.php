381834d88ad3ee1a31a484b4fee889b2bb5aeae5
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-01-29 13:39:07 +0100

    Add comments

--- php/Downloader.php
+++ php/Downloader.php
@@ -9,76 +9,127 @@ class Downloader {
 
     private $url;
     private $headers;
-    private $info;
+    private $info = false;
     private $curl;
     private $data;
 
     private $etag = "";
-    protected $etag_array = [];
-    protected $etag_path = "";
+    private $etag_array = [];
+    private $etag_path = "";
 
     public function __construct($url)
     {
+        // Set the url
         $this->url = $url;
+
+        // Set the path to the etags file
         $this->etag_path = storage_path() . "/import/etags";
+
+        // Init curl
         $this->curl = curl_init($this->url);
+
+        // Save the headers for the external file
         $this->headers = get_headers($this->url, true);
 
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, false);
 
+        // Check if the etags file exists
         if (File::exists($this->etag_path))
         {
+            // Set the etags array
             $this->etag_array = unserialize(File::get($this->etag_path));
 
+            // Find the etag for the current url
             if (array_key_exists($this->url, $this->etag_array))
             {
+                // Set the etag for this instance
                 $this->etag = $this->etag_array[$this->url];
             }
         }
 
+        // Set the etag in the header
         curl_setopt($this->curl, CURLOPT_HTTPHEADER, [
             'If-None-Match: ' . $this->etag
         ]);
     }
 
+    /**
+     * Get the size of the download in MB's
+     *
+     * @returns String
+     */
     public function getSize()
     {
         return ($this->headers['Content-Length'] / 1000000) . " MB";
     }
 
+    /**
+     * Get the headers for the set url
+     *
+     * @returns Array
+     */
     public function getHeaders()
     {
         return $this->headers;
     }
 
+    /**
+     * Get the info from the curl transmission, 
+     * returns false if the download hasn't been executed yet
+     *
+     * @returns Array|false
+     */
     public function getInfo()
     {
         return $this->info;
     }
 
+    /**
+     * Download the file
+     *
+     * @returns Boolean
+     */
     public function download($name)
     {
+        // Save the returned data
         $this->data = curl_exec($this->curl);
+
+        // Set the download info
         $this->info = curl_getinfo($this->curl);
 
+        // Check for 304 Not Modified
         if ($this->notModified()) {
+            // Return false if the download has not been executed
             return false;
         } else {
+            // Save the ETag (Should check if it exists first...)
             $this->saveETags($this->headers['ETag']);
 
+            // Save the data to file
             File::put(storage_path() . "/import/" . $name, $this->data);
 
+            // Download was succesful
             return true;
         }
     }
-
+    
+    /**
+     * Check for a 304 Not Modified header
+     *
+     * @returns Boolean
+     */
     public function notModified()
     {
         return $this->info['http_code'] === 304;
     }
 
-    protected function saveETags($etag)
+    /**
+     * Save the etag to the etags array
+     *
+     * @returns Void
+     */
+    private function saveETags($etag)
     {
         $this->etag_array[$this->url] = $etag;
 
